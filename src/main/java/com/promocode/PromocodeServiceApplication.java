c51297d569194a21ed6c71a75eb8eb5b2a3ad90f
gpaul.renew@gmail.com <SuperPower@2020>
2021-10-24 01:32:22 -0400

    Start up component

---
+++ src/main/java/com/promocode/PromocodeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.promocode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromocodeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromocodeServiceApplication.class, args);
+    }
+
+}
