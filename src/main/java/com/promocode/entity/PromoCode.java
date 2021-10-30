5bf6149574bc943a5b3bd01156882270d6c1b8ab
gpaul.renew@gmail.com <SuperPower@2020>
2021-10-30 03:29:39 -0400

    Renamed,disabled MySQL SSL use

--- src/main/java/com/promocode/entity/PromoCode.java
+++ src/main/java/com/promocode/entity/PromoCode.java
@@ -14,6 +14,39 @@ public class PromoCode {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String promoCode;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPromoCode() {
+        return promoCode;
+    }
+
+    public void setPromoCode(String promoCode) {
+        this.promoCode = promoCode;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(BigDecimal discount) {
+        this.discount = discount;
+    }
+
+    public String getExpDate() {
+        return expDate;
+    }
+
+    public void setExpDate(String expDate) {
+        this.expDate = expDate;
+    }
+
     private BigDecimal discount;
     private String expDate;
 
